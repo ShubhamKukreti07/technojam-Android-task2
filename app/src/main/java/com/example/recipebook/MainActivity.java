0b6acf9bd8e5a3f4fa3a9cf692ccd1b3fe0b6fcd
deeptikukreti <deeptikukreti98@gmail.com>
2021-11-25 23:29:44 +0530

    check git

--- app/src/main/java/com/example/recipebook/MainActivity.java
+++ app/src/main/java/com/example/recipebook/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rvRecipe=findViewById(R.id.rvRecipe);
+
         setupFireStore();
     }
 
